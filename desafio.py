25aca18388fda431d73de7fc4198f9a7c84029a8
Nelis Junior <nelisxxi@gmail.com>
2022-04-22 22:13:22 -0300

    Projeto renomeado

---
+++ desafio.py
@@ -0,0 +1,9 @@
+x = int(input())
+n= list()
+
+# TODO: Complete os espaços em branco com uma solução possível para o problema.
+
+for i in range (10):
+    n.append(    )
+    x=    
+    print("    = ".format(i), n[i])
